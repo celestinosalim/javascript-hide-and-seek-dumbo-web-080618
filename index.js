1879f27808d5f918316626e17fbf6d2633855311
celestinosalim <celestino.salim@gmail.com>
2018-10-09 23:47:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+// const getFirstSelector = ()=> {document.getElementById(selector)}
+// debugger
 
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.getElementById("nested").querySelector(".target");
+}
+
+function deepestChild() {
+  return document.getElementById("grand-node").querySelectorAll("div")[3]
+}
+
+function increaseRankBy(n) {
+  let lists = document.getElementById("app").querySelectorAll("ul.ranked-list li")
+  for (let element of lists) {
+    element.innerHTML=parseInt(element.innerHTML)+n;
+  }
+}
